bcd28509a8e88003d63b4cf0eadf757fdaa1e7ab
SohanChougule <79154533+SohanChougule@users.noreply.github.com>
2021-07-02 15:34:38 +0530

    deleted SpringMVCDemo

--- SpringMVCdemo/src/main/java/com/example/demo/dao/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import java.util.List;
-import com.example.demo.model.EmployeeVO;
-
-public interface EmployeeDAO 
-{
-    public List<EmployeeVO> getAllEmployees();
-}
