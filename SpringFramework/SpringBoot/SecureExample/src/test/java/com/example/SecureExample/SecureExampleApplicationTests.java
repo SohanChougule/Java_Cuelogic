09f151518a65a86dc050bca4de0f94f3fd7c1585
SohanChougule <sohan.chougule@cuelogic.com>
2021-07-21 09:17:00 +0530

    spring boot security and aop before after

---
+++ SpringFramework/SpringBoot/SecureExample/src/test/java/com/example/SecureExample/SecureExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecureExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
